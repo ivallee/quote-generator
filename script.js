c443709c1009eac9dd4dd8c58fbed31df540e48b
Isaac Vallee <isaacvallee@gmail.com>
2017-11-14 17:48:26 +0000

    get quote functio

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 $( document ).ready(function() {
   
-  $('#get-quote').on('click', function() {
-    console.log('sup');
-  });
+  function getQuote() {
+    $.getJSON('http://quotes.stormconsultancy.co.uk/random.json', function( data ) {
+      
+      var quote = "\"" + data.quote + "\"";
+      var author = "-" + data.author;
 
+      $('.quote').html(quote);
+      $('.author').html(author);
+
+    });
+  };
+
+  getQuote();
+  
+  $('#get-quote').on('click', function() {
+
+    getQuote();
+
+  });
+  
   $('#tweet').on('click', function() {
     console.log('twit!');
   });
-
+  
 });
