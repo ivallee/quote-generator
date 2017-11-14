3328101f4805f379e3588080677dff258d945e2c
Isaac Vallee <isaacvallee@gmail.com>
2017-11-14 17:21:00 +0000

    add button events

---
+++ script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  
+  $('#get-quote').on('click', function() {
+    console.log('sup');
+  });
+
+  $('#tweet').on('click', function() {
+    console.log('twit!');
+  });
+
+});
